2bd6aa8594aecc49106f34e38be22f5198775741
ramses2099 <ramses2099@gmail.com>
2024-02-10 10:54:34 -0400

    commit all changes

--- src/main/java/com/jprograming/form/FrmSignUpOne.java
+++ src/main/java/com/jprograming/form/FrmSignUpOne.java
@@ -5,38 +5,31 @@ import com.jprograming.db.DbConnectionPostgress;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
 
-public class FrmSignUpOne extends JFrame {
+public class FrmSignUpOne extends JFrame implements ActionListener {
 
     public final static String TITLE_FROM = "Sign UP";
     private JPanel pnlPrincipal;
     private JLabel lblTitle;
-    private JLabel lblDetails;
-    private JTextField txtFirstName;
-    private JLabel lblFirstName;
-    private JLabel lblLastName;
-    private JTextField txtLastName;
-    private JLabel lblDateOfBirth;
-    private JLabel Gender;
-    private JLabel lblEmail;
-    private JTextField txtEmail;
-    private JLabel lblAddress;
-    private JTextField txtAddress;
-    private JLabel lblCity;
-    private JTextField txtCity;
-    private JLabel lblState;
-    private JTextField txtState;
-    private JLabel lblPinCode;
-    private JTextField txtPinCode;
+    private JLabel lblTitle2;
+    private JPanel pnlTitle;
+    private JPanel pnlButton;
     private JRadioButton rbtnMale;
     private JRadioButton rbtnFemale;
     private JRadioButton rbtnMarrie;
     private JRadioButton rbtnSingle;
     private JButton btnNext;
+    private JPanel pnlCard;
+    private JPanel pnlCard1;
+    private JPanel pnlCard2;
+    private JPanel pnlCard3;
     private ButtonGroup btngrpStatusMarital;
     private ButtonGroup btngrpGender;
     private Random random;
+    private static int PAGE = 1;
 
     public FrmSignUpOne() {
         setContentPane(pnlPrincipal);
@@ -51,10 +44,14 @@ public class FrmSignUpOne extends JFrame {
         // Control set
         setContentPane(pnlPrincipal);
         getContentPane().setBackground(Color.WHITE);
+        pnlButton.setBackground(Color.WHITE);
+        pnlTitle.setBackground(Color.WHITE);
 
         random = new Random();
         Long formNumber = (Math.abs((random.nextLong() % 900L) + 1000L));
         lblTitle.setText(String.format("APPLICATION NO.:%s", formNumber));
+        PAGE = 1;
+        lblTitle2.setText(String.format("Page %s: Personal Details", PAGE));
         //
         btngrpGender = new ButtonGroup();
         btngrpGender.add(rbtnMale);
@@ -66,24 +63,49 @@ public class FrmSignUpOne extends JFrame {
         //
         btnNext.setBackground(Color.BLACK);
         btnNext.setForeground(Color.WHITE);
+        btnNext.addActionListener(this);
+
 
         try {
             ImageIcon imageIcon = new ImageIcon(ImageIO.read(getClass().getResourceAsStream("/icons/logo.jpg")));
             setIconImage(imageIcon.getImage());
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
 
+    }
 
-        try {
-            DbConnectionPostgress connectionPostgress = new DbConnectionPostgress();
-            connectionPostgress.testConnection();
-        }catch (Exception e){
-            System.out.println(e.getMessage());
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (PAGE == 1) {
+            pnlCard.removeAll();
+            pnlCard.add(pnlCard1);
+            pnlCard.repaint();
+            pnlCard.revalidate();
+            lblTitle2.setText(String.format("Page %s: Personal Details", PAGE));
+            PAGE += 1;
+        } else if (PAGE == 2) {
+            pnlCard.removeAll();
+            pnlCard.add(pnlCard2);
+            pnlCard.repaint();
+            pnlCard.revalidate();
+            lblTitle2.setText(String.format("Page %s: Personal Details", PAGE));
+            PAGE += 1;
+        } else if (PAGE == 3) {
+            pnlCard.removeAll();
+            pnlCard.add(pnlCard3);
+            pnlCard.repaint();
+            pnlCard.revalidate();
+            lblTitle2.setText(String.format("Page %s: Personal Details", PAGE));
+            PAGE += 1;
+        } else if (PAGE == 4) {
+            PAGE = 1;
+            lblTitle2.setText(String.format("Page %s: Personal Details", PAGE));
+            pnlCard.removeAll();
+            pnlCard.add(pnlCard1);
+            pnlCard.repaint();
+            pnlCard.revalidate();
         }
-
-
-
     }
 }
